9611bd2839cee0b4e38679d0fd0a01a3832b05b5
Ubuntu <ubuntu@ip-172-31-36-17.ap-southeast-2.compute.internal>
2021-08-25 09:55:30 +0000

    feat: added user data

--- lib/user-data.ts
+++ lib/user-data.ts
@@ -1,9 +1,27 @@
+const goVersion = '1.17';
+const nvmVersion = '0.38.0';
+const nodeVersion = '14.17.5';
+
 export function userData(): string {
   return [
     '#!/bin/bash',
+    // Pipe logs.
     'exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1',
+    // Install updates.
     'apt update',
     'apt upgrade -y',
-    'mkdir -p /home/ubuntu/code'
+    // Install Go.
+    `wget https://golang.org/dl/go${goVersion}.linux-amd64.tar.gz`,
+    'rm -rf /usr/local/go',
+    `tar -C /usr/local -xzf go${goVersion}.linux-amd64.tar.gz`,
+    'echo "" >> /home/ubuntu/.profile',
+    'echo "# Add Go bin directory to path." >> /home/ubuntu/.profile',
+    'echo "export PATH=\$PATH:/usr/local/go/bin" >> /home/ubuntu/.profile',
+    // Install NVM.
+    `curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${nvmVersion}/install.sh | bash`,
+    `nvm install v${nodeVersion}`,
+    // Create code directory.
+    'mkdir -p /home/ubuntu/code',
+    'chown ubuntu:ubuntu /home/ubuntu/code'
   ].join('\n')
 }
